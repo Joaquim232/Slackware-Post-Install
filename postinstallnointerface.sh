d8efa85408f4a0c14bf73fa493009069982974a5
Joaquim Miguel <74877588+Joaquim232@users.noreply.github.com>
2021-04-04 13:03:22 -0300

    Add files via upload

---
+++ postinstallnointerface.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+nano /etc/slackpkg/mirrors
+slackpkg update
+slackpkg upgrade firefox zlib
+wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
+installpkg sbopkg-0.38.1-noarch-1_wsr.tgz
+sbopkg
+sbopkg -i python3
+sbopkg -i python-urllib3
+sbopkg -i slpkg
+slpkg update
+slpkg -s sbo hack-font-ttf
+mkdir /home/joaquim/.themes
+mkdir /home/joaquim/.icons
+slpkg -s sbo feh
+git clone https://www.github.com/9wm/9wm
+cd 9wm
+make
+make install
+cd
