8ec93b1ef813b84a3279d476d5cf044775e496b0
Joaquim Miguel <74877588+Joaquim232@users.noreply.github.com>
2021-04-12 20:51:07 -0300

    Update postinstallnointerface.sh

--- postinstallnointerface.sh
+++ postinstallnointerface.sh
@@ -4,6 +4,7 @@ slackpkg update
 slackpkg upgrade firefox zlib
 wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
 installpkg sbopkg-0.38.1-noarch-1_wsr.tgz
-slpkg -s sbo hack-font-ttf
+wget https://software.jaos.org/slackpacks/slackware64-14.2/slapt-get/slapt-get-0.11.2-x86_64-1.txz
+installpkg slapt-get-0.11.2-x86_64-1.txz
 mkdir /home/joaquim/.themes
 mkdir /home/joaquim/.icons
