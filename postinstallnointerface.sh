343151974ea50fa0446ccbc87386e0def79ec357
Joaquim Miguel <74877588+Joaquim232@users.noreply.github.com>
2021-04-12 20:45:09 -0300

    Update postinstallnointerface.sh

--- postinstallnointerface.sh
+++ postinstallnointerface.sh
@@ -4,11 +4,6 @@ slackpkg update
 slackpkg upgrade firefox zlib
 wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
 installpkg sbopkg-0.38.1-noarch-1_wsr.tgz
-sbopkg
-sbopkg -i python3
-sbopkg -i python-urllib3
-sbopkg -i slpkg
-slpkg update
 slpkg -s sbo hack-font-ttf
 mkdir /home/joaquim/.themes
 mkdir /home/joaquim/.icons
